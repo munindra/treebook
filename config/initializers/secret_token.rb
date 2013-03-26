696ab01ba5c7f11969d64b4b0bb2b0e91342518a
munindra <munindra.nath@gmail.com>
2013-03-26 15:47:53 -0400

    Created rail application and addes statis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '63c41622b3dc65f624b2dd411e1dcb249191f5319d0aef80eb73ab442f31d7e3095c617b900d6239aeee83a886ce4e8e13487484535c3f5ecfaa974052ef9099'
